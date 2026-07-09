107de2767cb67a56834fc0316fe06c1f8665da13
Soupal Purkayastha <soupalpurkayastha@gmail.com>
2026-07-09 16:42:59 +0530

    Overall layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { PieChart, Pie, Cell, ResponsiveContainer, Tooltip, Legend } from 'recharts';
+
+function App(){
+  //1. storage & state engine
+
+  //2.input element controllers
+
+  //3.Mathematical metrics
+
+  //4. Recharts Layout Data transformer
+
+  //5.Interaction Methods
+
+  //6.Presentation Interface (JSX Renger)
+}
